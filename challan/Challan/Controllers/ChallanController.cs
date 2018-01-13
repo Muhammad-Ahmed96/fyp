1a1b8695b751185246d5b3172d6541fe02a649d5
Muhammad Ahmed <ahmed.hanjra96@gmail.com>
2018-01-13 19:07:21 +0500

    fyp1
    first release

---
+++ challan/Challan/Controllers/ChallanController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Challan.Controllers
+{
+    public class ChallanController : Controller
+    {
+        //
+        // GET: /Challan/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
